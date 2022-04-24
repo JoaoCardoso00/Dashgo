796c883e438e04cc26fa9fc40c0f48b6498950d3
JoaoCardoso00 <jvcq13@gmail.com>
2022-04-23 21:03:15 -0300

    added react query mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -15,6 +15,10 @@ import Link from "next/link";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -36,6 +40,26 @@ const CreateUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(CreateUserFormSchema),
   });
@@ -43,8 +67,8 @@ export default function CreateUser() {
   const { errors } = formState;
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    console.log(data);
+    await createUser.mutateAsync(data);
+    router.push("/users");
   };
 
   return (
@@ -71,16 +95,18 @@ export default function CreateUser() {
           <VStack spacing="8">
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input
-                name="name"
+                name="nome"
                 label="Nome completo"
                 {...register("name")}
                 error={errors.name}
+                type="text"
               />
               <Input
                 name="email"
                 label="E-mail"
                 {...register("email")}
                 error={errors.email}
+                type="email"
               />
             </SimpleGrid>
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
